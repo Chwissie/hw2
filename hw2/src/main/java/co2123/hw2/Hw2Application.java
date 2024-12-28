77c9a08f405b726ab2bef3409810f1e3a81bd17d
Kia-ora <keir.almano10@gmail.com>
2024-12-28 19:56:43 +0000

    partial task 5
    task 5 half done

--- hw2/src/main/java/co2123/hw2/Hw2Application.java
+++ hw2/src/main/java/co2123/hw2/Hw2Application.java
@@ -7,21 +7,58 @@ import co2123.hw2.repo.BakeryRepository;
 import co2123.hw2.repo.BreadRepository;
 import co2123.hw2.repo.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Hw2Application {
+import java.util.ArrayList;
+import java.util.List;
 
+@SpringBootApplication
+public class Hw2Application implements CommandLineRunner {
     @Autowired
     private BakeryRepository bakeryRepository;
+
+    @Autowired
     private BreadRepository breadRepository;
+
+    @Autowired
     private IngredientRepository ingredientRepository;
 
-    public static void main(String[] args) {
-        SpringApplication.run(Hw2Application.class, args);
+    public static List<Bakery> bakeryList = new ArrayList<>();
+    public static List<Bread> breadList = new ArrayList<>();
+    public static List<Ingredient> ingredientList = new ArrayList<>();
+
+    @Override
+    public void run(String... args) throws Exception {
         Bakery bakery = new Bakery();
         Bread bread = new Bread();
         Ingredient ingredient = new Ingredient();
+
+
+        bakeryList.add(bakery);
+        breadList.add(bread);
+        ingredientList.add(ingredient);
+
+
+        bakery.setAddress("here");
+        bakery.setID(1);
+        bakery.setNewest(bread);
+        bakery.setBreads(breadList);
+
+        bread.setName("Bread");
+        bread.setFilling(ingredient);
+        bread.setBakeries(bakeryList);
+        bread.setIngredients(ingredientList);
+
+        ingredient.setAmount(5);
+        ingredient.setIdentifier(1);
+
+        breadRepository.save(bread);
+        ingredientRepository.save(ingredient);
+        bakeryRepository.save(bakery);
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Hw2Application.class, args);
     }
 }
--- hw2/src/main/java/co2123/hw2/controller/ListController.java
+++ hw2/src/main/java/co2123/hw2/controller/ListController.java
@@ -1,4 +1,26 @@
 package co2123.hw2.controller;
+import co2123.hw2.Hw2Application;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 public class ListController {
+    Hw2Application hw2Application;
+
+    @GetMapping("/listBakery")
+    public String listBakery(Model model) {
+        model.addAttribute("data", Hw2Application.bakeryList);
+        return "list";
+    }
+
+    @GetMapping("/listBread")
+    public String listBread(Model model) {
+        model.addAttribute("data", Hw2Application.breadList);
+        return "list";
+    }
+
+    @GetMapping("/listIngredient")
+    public String listIngredient(Model model) {
+        model.addAttribute("data", Hw2Application.ingredientList);
+        return "list";
+    }
 }
